e5d7589c33d133ce84f099ac59cc8a019b3a0d3c
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-02-22 23:57:28 +0500

    spinner

--- src/components/sidebars/RightBar.js
+++ src/components/sidebars/RightBar.js
@@ -13,16 +13,18 @@ const RightBar = () => {
   }
   return (
     <RightContainer>
-      <Info>
-        <h4>{data.data.country}</h4>
-        <Flag src={data.data.countryInfo?.flag} />
-      </Info>
-      <Wrapper>
-        <Cases text="Total cases" color="red" number={data.data.cases} />
-        <Cases text="Vaccination" color="blue" number={vaccine} />
-        <Cases text="Recovered" color="green" number={data.data.recovered} />
-        <Cases text="Deaths" color="black" number={data.data.deaths} />
-      </Wrapper>
+    { data.isLoading? 'loading...' : <>
+        <Info>
+          <h4>{data.data.country}</h4>
+          <Flag src={data.data.countryInfo?.flag} />
+        </Info>
+        <Wrapper>
+          <Cases text="Total cases" color="red" number={data.data.cases} />
+          <Cases text="Vaccination" color="blue" number={vaccine} />
+          <Cases text="Recovered" color="green" number={data.data.recovered} />
+          <Cases text="Deaths" color="black" number={data.data.deaths} />
+        </Wrapper>
+      </>}
     </RightContainer>
   );
 };
