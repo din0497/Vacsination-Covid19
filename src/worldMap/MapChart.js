7d2d734ad137b99601e1df20a72ac7ece486746d
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-02-28 21:18:20 +0500

    debugged

--- src/worldMap/MapChart.js
+++ src/worldMap/MapChart.js
@@ -12,15 +12,15 @@ import { getTooltip } from "../Redux/actions/actions";
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-// const rounded = (num) => {
-//   if (num > 1000000000) {
-//     return Math.round(num / 100000000) / 10 + "Bn";
-//   } else if (num > 1000000) {
-//     return Math.round(num / 100000) / 10 + "M";
-//   } else {
-//     return Math.round(num / 100) / 10 + "K";
-//   }
-// };
+const rounded = (num) => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + "M";
+  } else {
+    return Math.round(num / 100) / 10 + "K";
+  }
+};
 
 const MapChart = () => {
   const dispatch = useDispatch();
